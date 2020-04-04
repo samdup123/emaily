ccf1729a514758dc572b5eb23c07aac57399fe52
Sam DuPlessis <samdup123@gmail.com>
2020-04-04 19:15:23 -0400

    prod environment

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientId: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoUri: process.env.MONGO_URI,
+  cookieKey1: process.env.COOKIE_KEY_1,
+  cookieKey2: process.env.COOKIE_KEY_2,
+  apiKey: process.env.API_KEY
+}
