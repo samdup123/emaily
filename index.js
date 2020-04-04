5caaf1de506dc9653fb808a72c5ba40948b7f809
Sam DuPlessis <samdup123@gmail.com>
2020-04-04 13:37:14 -0400

    I done logged in bruh

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const Express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
 const app = Express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey1, keys.cookieKey2],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./models/User');
 require('./services/passportConfig');
 require('./routes/authRoutes')(app);
+
+mongoose.connect(keys.mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- services/passportConfig.js
+++ services/passportConfig.js
@@ -1,6 +1,17 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => done(null, user));
+});
 
 // something: AIzaSyCDCSOE1U5KnjUzOCGA6HgjiFF3bksNAzQ
 passport.use(
@@ -11,7 +22,15 @@ passport.use(
       callbackURL: '/auth/google/callback',
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken, refreshToken, profile, done);
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser == null || existingUser == undefined) {
+          new User({ googleId: profile.id })
+            .save()
+            .then((user) => done(null, user));
+        } else {
+          done(null, existingUser);
+        }
+      });
     }
   )
 );
